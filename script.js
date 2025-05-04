5ecfbcb41357d96052ba8480e86300b2b0db9f58
Lucas Moro Iroldi <134328512+lucas-moro@users.noreply.github.com>
2025-05-04 13:12:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao =document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click",a )
+
+    function a(){
+    mensagem.textContent = "Ola , bem vindo!";
+};
